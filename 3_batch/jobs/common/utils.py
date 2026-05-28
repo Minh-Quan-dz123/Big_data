e1d21b99e6af9e8aba6d6c337bf55cb9615785d4
Hoàng Khắc Tiến <161594088+TienCE04@users.noreply.github.com>
2026-05-28 23:03:35 +0700

    update current time

--- 3_batch/jobs/common/utils.py
+++ 3_batch/jobs/common/utils.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 def current_time() -> datetime:
     """
-    Returns the fixed simulated current time for the dataset (2025-11-14 23:18:00).
-    This is used because the dataset tables are from 2025.
+    Returns the current time.
     
     Returns:
-        datetime: A datetime object set to 2025-11-14 23:18:00
+        datetime: A datetime object of the current time.
     """
-    return datetime(2025, 11, 14, 23, 18, 0)
+    return datetime.now()
